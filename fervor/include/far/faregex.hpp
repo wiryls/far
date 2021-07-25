325f67aa1913cb4634b2928105c7e3ce5320dce7
wiryls <wiryls@gmx.com>
2021-07-25 20:02:32 +0800

    chore: plan to use ranges API

--- fervor/include/far/faregex.hpp
+++ fervor/include/far/faregex.hpp
@@ -1,41 +1,65 @@
 #pragma once
 
+/// Note:
+/// this file is base on RUST version:
+/// https://github.com/wiryls/far/blob/deprecated/gtk4-rs/src/far/diff.rs and
+/// https://github.com/wiryls/far/blob/deprecated/gtk4-rs/src/far/faregex.rs
+
 #include <concepts>
 #include <iterator>
 #include <ranges>
+#include <variant>
+#include <vector>
 #include <string>
+#include <string_view>
 #include <regex>
 
-namespace far
+namespace far { namespace cep
 {
-    //// common concepts
+    /// common concepts for far
 
     template<typename C>
     concept char_type
         = std::same_as<typename std:: char_traits<C>::char_type, C>
        && std::same_as<typename std::regex_traits<C>::char_type, C>;
 
-    template<typename T, typename C>
-    concept string_type
-        = std::ranges::forward_range<T>
-       && std::convertible_to
-            < std::ranges::range_value_t<T>
+    template<typename S, typename C>
+    concept string_like
+       = std::ranges::forward_range<S>
+      && std::convertible_to
+            < std::ranges::range_value_t<S>
             , typename std::char_traits<C>::char_type >;
+}}
 
-    //// faregex with deduction guide
+namespace far
+{
+    //// changes
+    template<far::cep::char_type C>
+    using change = std::variant
+        < std::basic_string_view<C>
+        , std::basic_string_view<C>
+        , std::basic_string     <C>
+        >;
 
-    template<far::char_type C>
+    //// iterator
+    template<far::cep::char_type C>
+    class iterator;
+
+    //// faregex (with deduction guide)
+    template<far::cep::char_type C>
     class faregex;
 
     template<std::ranges::forward_range P, std::ranges::forward_range R>
     faregex(P &&, R &&, bool = false) -> faregex<std::ranges::range_value_t<P>>;
 }
 
-template<far::char_type C>
+//// implementation starts from here
+
+template<far::cep::char_type C>
 class far::faregex
 {
 public:
-    template<far::string_type<C> P, far::string_type<C> R>
+    template<far::cep::string_like<C> P, far::cep::string_like<C> R>
     faregex(P && pattern, R && replace, bool ignore_case = false);
 
 public:
@@ -50,20 +74,21 @@ private:
     std::basic_string<C> replace;
 };
 
-template<far::char_type C>
-template<far::string_type<C> P, far::string_type<C> R> inline
+template<far::cep::char_type C>
+template<far::cep::string_like<C> P, far::cep::string_like<C> R> inline
 far::faregex<C>::faregex(P && pattern, R && replace, bool ignore_case)
     : pattern
         ( std::ranges::begin(std::forward<P>(pattern))
         , std::ranges::end  (std::forward<P>(pattern))
-        , static_cast<std::regex::flag_type>((ignore_case ? std::regex::icase : 0) | std::regex::ECMAScript) )
+        , static_cast<std::regex::flag_type>
+            ( (ignore_case ? std::regex::icase : 0)
+            | (std::regex::ECMAScript) ) )
     , replace
         ( std::ranges::begin(std::forward<R>(replace))
         , std::ranges::end  (std::forward<R>(replace)) )
 {}
 
-
-template<far::char_type C>
+template<far::cep::char_type C>
 template<std::ranges::bidirectional_range R> inline auto
 far::faregex<C>::faregex::operator()(R && container) -> void
 {
@@ -72,7 +97,7 @@ far::faregex<C>::faregex::operator()(R && container) -> void
         , std::ranges::end  (std::forward<R>(container)) );
 }
 
-template<far::char_type C>
+template<far::cep::char_type C>
 template<std::bidirectional_iterator I> inline auto
 far::faregex<C>::faregex::operator()(I const & first, I const & last) -> void
 {
@@ -80,7 +105,7 @@ far::faregex<C>::faregex::operator()(I const & first, I const & last) -> void
 
     auto head = iterator_t(first, last, pattern);
     auto tail = iterator_t();
-    for ( ; head != tail; ++head)
+    for (; head != tail; ++head)
     {
         auto const & match = *head;
         (void) match;
--- fervor/tests/functional_tests/faregex_test.cpp
+++ fervor/tests/functional_tests/faregex_test.cpp
@@ -1,17 +1,19 @@
 #include <catch2/catch_test_macros.hpp>
 #include <far/faregex.hpp>
 
-TEST_CASE("constructable", "[faregex]")
+TEST_CASE("constructor, trait, deduction guide", "[faregex]")
 {
     // char type is limited by regex_traits
     // https://en.cppreference.com/w/cpp/regex/regex_traits
-    SECTION("char")
+    SECTION("char const & [N]")
     {
+        static_assert(far::cep::char_type<char>);
         auto f = far::faregex("", "");
         f("");
     }
-    SECTION("wchar_t")
+    SECTION("std::wstring")
     {
+        static_assert(far::cep::string_like<std::wstring, wchar_t>);
         auto w = std::wstring();
         auto f = far::faregex(w, w);
         f(w);
