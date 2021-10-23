d77613cf59794f40d2c2c5e91c7d2777fb725638
wiryls <wiryls@gmx.com>
2021-10-23 18:22:18 +0800

    feat: create a task lib as dll

---
+++ lib/exports/task/src/main.cpp
@@ -0,0 +1,11 @@
+#include <functional>
+#include <filesystem>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <format>
+
+#include <far/exec.hpp>
+#include <far/task.hpp>
+
