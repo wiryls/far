ed88610c8e9f35bd60021eb32653f0056fd50c0d
wiryls <wiryls@gmx.com>
2021-11-03 02:08:36 +0800

    feat: use enum as stat

--- List.cs
+++ List.cs
@@ -1,28 +1,87 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.Globalization;
+using System.Windows.Data;
 
 namespace FAR
 {
-    public class Item
-    {
-        public string Stat { get; set; } // or enum
-        public string View { get; set; } // rich text
-        public string Path { get; set; } // rich text
-    }
-
     public class List : ObservableCollection<Item>
     {
         public List() : base()
         {
-            Add(new Item { Stat = "?", Path = "/user/bin", View = "bin" });
-            Add(new Item { Stat = "x", Path = "/user/local", View = "local" });
-            Add(new Item { Stat = "o", Path = "/", View = "" });
-            Add(new Item { Stat = "!", Path = "/etc/apt", View = "apt" });
-            Add(new Item { Stat = "f", Path = "/etc", View = "etc" });
-            Add(new Item { Stat = "a", Path = "/user/local/bin", View = "bin" });
+            Add(new Item { Stat = Status.Todo, Path = "/user/bin", View = "bin" });
+            Add(new Item { Stat = Status.Fail, Path = "/user/local", View = "local" });
+            Add(new Item { Stat = Status.Fail, Path = "/", View = "" });
+            Add(new Item { Stat = Status.Done, Path = "/etc/apt", View = "apt" });
+            Add(new Item { Stat = Status.Fail, Path = "/etc", View = "etc" });
+            Add(new Item { Stat = Status.Done, Path = "/user/local/bin", View = "bin" });
         }
+    }
+
+    public class Item
+    {
+        public Status Stat { get; set; }
+        public string View { get; set; }
+        public string Path { get; set; }
+    }
+
+    public enum Status
+    {
+        Todo,
+        Done,
+        Fail,
+        Lost,
+    }
+
+    [ValueConversion(typeof(Status), typeof(string))]
+    public class StatusConverter : IValueConverter
+    {
+        private const string todo = "TODO";
+        private const string done = "DONE";
+        private const string fail = "FAIL";
+        private const string lost = "LOST";
+
+        public object Convert(object value, Type target, object parameter, CultureInfo culture)
+        {
+            return (Status)value switch
+            {
+                Status.Todo => todo,
+                Status.Done => done,
+                Status.Fail => fail,
+                Status.Lost => lost,
+                _ => todo,
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value as string switch
+            {
+                todo => Status.Todo,
+                done => Status.Done,
+                fail => Status.Fail,
+                lost => Status.Lost,
+                _ => Status.Todo,
+            };
+        }
+    }
+
+    public class Cahnge : List<Operation> { }
+
+    public class Operation
+    {
+        public enum Action
+        {
+            Retain,
+            Insert,
+            Delete,
+        }
+
+        public Action Type;
+        public string Text;
     }
 
     // references:
