eb796cbf4a55c55c43712c9bb32f2747f9775eff
wiryls <wiryls@gmx.com>
2021-11-20 14:25:33 +0800

    feat: column-view is sortable now

--- List.cs
+++ List.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace FAR
 {
@@ -13,15 +15,17 @@ namespace FAR
             Add(new Item { Stat = Status.Fail, Path = "/", View = new Change { new Operation { Type = Operation.Action.Retain, Text = "nooo" } } });
             Add(new Item { Stat = Status.Done, Path = "/etc/apt", View = new Change { new Operation { Type = Operation.Action.Retain, Text = "nooo" } } });
             Add(new Item { Stat = Status.Fail, Path = "/etc", View = new Change { new Operation { Type = Operation.Action.Delete, Text = "nooo" } } });
-            Add(new Item { Stat = Status.Done, Path = "/user/local/bin", View = new Change { new Operation { Type = Operation.Action.Insert, Text = "nooo" } } });
+            Add(new Item { Stat = Status.Done, Path = "/user/local/bin", View = new Change { new Operation { Type = Operation.Action.Insert, Text = "nooo" }, new Operation { Type = Operation.Action.Delete, Text = "what" } } });
         }
     }
 
     public class Item
     {
-        public Status Stat { get; set; }
-        public Change View { get; set; }
-        public string Path { get; set; }
+        public Status Stat { get; set; } // task status
+        public Change View { get; set; } // preview
+        public string Path { get; set; } // path to directory
+        public string Source { get; set; } // the source name
+        public string Target { get; set; } // the target name
     }
 
     public enum Status
@@ -32,7 +36,21 @@ namespace FAR
         Lost,
     }
 
-    public class Change : List<Operation> { }
+    public class Change : List<Operation>, IComparer<Change>
+    {
+        public int Compare(Change x, Change y)
+        {
+            var l = x
+                .Where(x => x.Type != Operation.Action.Delete)
+                .SelectMany(x => x.Text.AsEnumerable());
+            var r = y
+                .Where(x => x.Type != Operation.Action.Delete)
+                .SelectMany(x => x.Text.AsEnumerable());
+            return Enumerable
+                .Zip(l, r, (l, r) => l - r)
+                .FirstOrDefault(x => x != 0);
+        }
+    }
 
     public class Operation
     {
