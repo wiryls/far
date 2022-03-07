904850e558a9a6d03805898d612491b18ded83cf
wiryls <wiryls@gmx.com>
2022-03-08 01:38:33 +0800

    chore: do some cleaning and ready to use sqlite

--- Fx/Diff/Types.cs
+++ Fx/Diff/Types.cs
@@ -19,30 +19,26 @@
 
         public Diff(int capacity) : base(capacity) { }
 
-        public void Add(Action action, string text)
-        {
+        public void Add(Action action, string text) =>
             Add(new Operation { Type = action, Text = text });
-        }
 
-        public int Compare(Diff? x, Diff? y)
-        {
-            static IEnumerable<char> toChars(Diff? x) => x?
-                .Where(x => x.Type != Action.Delete)
-                .SelectMany(x => x.Text.AsEnumerable()) ?? Enumerable.Empty<char>();
-
-            return Enumerable
-                .Zip(toChars(x), toChars(y), (l, r) => l - r)
-                .FirstOrDefault(x => x != 0);
-        }
+        public int Compare(Diff? x, Diff? y) => Enumerable
+            .Zip(RetainInsertToChars(x), RetainInsertToChars(y), (l, r) => l - r)
+            .FirstOrDefault(x => x != 0);
 
         public string Source
         { 
-            get { return string.Concat(this.Where(x => x.Type != Action.Insert).Select(x => x.Text)); }
+            get => string.Concat(this.Where(x => x.Type != Action.Insert).Select(x => x.Text));
         }
 
         public string Target
         {
-            get { return string.Concat(this.Where(x => x.Type != Action.Delete).Select(x => x.Text)); }
+            get => string.Concat(this.Where(x => x.Type != Action.Delete).Select(x => x.Text));
         }
+
+        private static IEnumerable<char> RetainInsertToChars(Diff? x) => x?
+            .Where(x => x.Type != Action.Delete)
+            .SelectMany(x => x.Text.AsEnumerable()) ?? Enumerable.Empty<char>();
+
     }
 }
